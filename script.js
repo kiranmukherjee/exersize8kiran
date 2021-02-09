3b9512306f185f8b3930cf676416143198db22fb
kiranmukherjee <77300690+kiranmukherjee@users.noreply.github.com>
2021-02-08 23:17:57 -0800

    really trying

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-let time = 0
-
-button1 = document.getElementById("submission1")
-
-button2 = document.getElementById("submission2")
-
-button3 = document.getElementById("submission3")
-
-button1.onclick = function() {
-    let time = 5
+function user(choice){
+    user = choice;
 }
 
-button2.onclick = function() {
-    let time = 15
-}
+submit = document.getElementById("submissionbtn")
 
-button3.onclick = function() {
-    let time = 25
-}
+submit.onclick = test()
 
-
-if (time < 10) {
-    greeting = "Good morning";
-  } else if (time < 20) {
-    greeting = "Good day";
-  } else {
-    greeting = "Good evening";
-  }
+function test(){
+    console.log("You chose " + choice);
+}         
 
 
-console.log()
+
+
+
+
+
+// let time = 0
+
+// button1 = document.getElementById("submission1")
+
+// button2 = document.getElementById("submission2")
+
+// button3 = document.getElementById("submission3")
+
+// button1.onclick = function() {
+//     let time = 5
+// }
+
+// button2.onclick = function() {
+//     let time = 15
+// }
+
+// button3.onclick = function() {
+//     let time = 25
+// }
+
+
+// if (time < 10) {
+//     greeting = "Good morning";
+//   } else if (time < 20) {
+//     greeting = "Good day";
+//   } else {
+//     greeting = "Good evening";
+//   }
+
+
+// console.log()
